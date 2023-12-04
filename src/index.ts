ebcc71223b727540cb1c9fa7a9d7bf689f6e64a9
gustavovenerando <gvhenrique@hotmail.com>
2023-12-03 23:20:04 -0300

    new<asyncTasks>: added multiple sites tasks at the same time

--- src/index.ts
+++ src/index.ts
@@ -6,52 +6,60 @@ import AdblockerPlugin from "puppeteer-extra-plugin-adblocker";
 puppeteer.use(StealthPlugin());
 puppeteer.use(AdblockerPlugin({ blockTrackers: true}));
 
-
-//SELECTORS/INFO - KABUM 
-// const numProductPerPage = 100;
-// const mainUrl = "https://www.kabum.com.br/hardware/placa-de-video-vga?page_number=PAGE_NUM&page_size=100&facet_filters=&sort=most_searched";
-// const productTextSelector = 'div.sc-cdc9b13f-7:nth-child(INDEX) > a:nth-child(2) > div:nth-child(2) span[class="sc-d79c9c3f-0 nlmfp sc-cdc9b13f-16 eHyEuD nameCard"]';
-// const productCountSelector = "#listingCount";
-// const productSelector = ".productCard";
-
-
-// SELECTORS/INFO - PICHAU 
-const numProductPerPage = 36;
-const mainUrl = "https://www.pichau.com.br/hardware/placa-de-video?page=PAGE_NUM";
-const productTextSelector = 'div.MuiGrid-grid-xs-6:nth-child(INDEX) > a:nth-child(1) > div:nth-child(1) > div:nth-child(3) > h2:nth-child(1)';
-const productCountSelector = "div.MuiGrid-grid-lg-10 > div:nth-child(1) > div > div:nth-child(1) > div:nth-child(1) > div";
-const productSelector = 'a[data-cy="list-product"]';
+const siteArr = [
+    {
+        site: "pichau",
+        numProductPerPage: 36,
+        mainUrl: "https://www.pichau.com.br/hardware/placa-de-video?page=PAGE_NUM",
+        productTextSelector: 'div.MuiGrid-grid-xs-6:nth-child(INDEX) > a:nth-child(1) > div:nth-child(1) > div:nth-child(3) > h2:nth-child(1)',
+        productCountSelector : "div.MuiGrid-grid-lg-10 > div:nth-child(1) > div > div:nth-child(1) > div:nth-child(1) > div",
+        productSelector : 'a[data-cy="list-product"]',
+    },
+    {
+        site: "kabum",
+        numProductPerPage: 100,
+        mainUrl: "https://www.kabum.com.br/hardware/placa-de-video-vga?page_number=PAGE_NUM&page_size=100&facet_filters=&sort=most_searched",
+        productTextSelector: 'div.sc-cdc9b13f-7:nth-child(INDEX) > a:nth-child(2) > div:nth-child(2) span[class="sc-d79c9c3f-0 nlmfp sc-cdc9b13f-16 eHyEuD nameCard"]',
+        productCountSelector : "#listingCount",
+        productSelector : ".productCard",
+    },
+]
 
 async function start() {
     const browser = await puppeteer.launch({ headless: false });
-    const page = await browser.newPage();
-    await page.setViewport({
-        width: 1600,
-        height: 1200,
-    });
 
-    const numPages = await getNumPages(page, productCountSelector, numProductPerPage);
-    page.close();
-    console.log("Number of pages: ", numPages);
+    const urlArr = [];
+    for (let { numProductPerPage, mainUrl, productCountSelector, ...productSelectors } of siteArr) {
+        const page = await browser.newPage();
+        await page.setViewport({
+            width: 1600,
+            height: 1200,
+        });
+        const numPages = await getNumPages(page, productCountSelector, numProductPerPage, mainUrl);
+        page.close();
+        console.log("Number of pages: ", numPages);
 
-    const urlArr = getUrlArr(numPages);
+        urlArr.push(...getUrlArr(numPages, mainUrl, productSelectors));
+    }
 
-    const result = await Promise.all(urlArr.map(url => extracPageData(browser, url)));
+    const result = await Promise.all(urlArr.map(elem => extracPageData(browser, elem.url, elem.productTextSelector, elem.productSelector)));
 
     console.log("Final Result: ", result);
 }
 
-function getUrlArr(numPages: number): string[]{
+function getUrlArr(numPages: number, mainUrl: string, productSelectors: any): any[]{
     const urlArr = [];
     for (let pageNum = 1; pageNum <= numPages; pageNum++) {
         const url = mainUrl.replace("PAGE_NUM", `${pageNum}`);
-        urlArr.push(url);
+        urlArr.push({url, ...productSelectors});
     }
+
+    // console.log("URL ARR: ", urlArr);
 
     return urlArr;
 }
 
-async function extracPageData(browser: Browser ,url: string){
+async function extracPageData(browser: Browser ,url: string, productTextSelector: string, productSelector: string){
     const page = await browser.newPage();
     await page.setViewport({
         width: 1600,
@@ -99,7 +107,7 @@ async function extractProductsData(page: Page, pageSelector: string): Promise<st
 }
 
 
-async function getNumPages(page: Page, pageSelector: string, numProductPerPage: number): Promise<number>{
+async function getNumPages(page: Page, pageSelector: string, numProductPerPage: number, mainUrl: string): Promise<number>{
     const initialUrl = mainUrl.replace("PAGE_NUM", "1");
     await page.goto(initialUrl);
 
