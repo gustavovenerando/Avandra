089a04566e8f3334008e87411b257a28251e0075
gustavovenerando <gvhenrique@hotmail.com>
2023-12-05 23:59:19 -0300

    v0.2.2: cleaning code, changing variables names and removed some comments

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,7 @@ const siteArr = [
         mainUrl: "https://www.pichau.com.br/hardware/placa-de-video?page=PAGE_NUM",
         productTextSelector: 'div.MuiGrid-grid-xs-6:nth-child(INDEX) > a:nth-child(1) > div:nth-child(1) > div:nth-child(3) > h2:nth-child(1)',
         productCountSelector : "div.MuiGrid-grid-lg-10 > div:nth-child(1) > div > div:nth-child(1) > div:nth-child(1) > div",
-        productSelector : 'a[data-cy="list-product"]',
+        productCardSelector : 'a[data-cy="list-product"]',
     },
     {
         site: "kabum",
@@ -21,7 +21,7 @@ const siteArr = [
         mainUrl: "https://www.kabum.com.br/hardware/placa-de-video-vga?page_number=PAGE_NUM&page_size=100&facet_filters=&sort=most_searched",
         productTextSelector: 'div.sc-cdc9b13f-7:nth-child(INDEX) > a:nth-child(2) > div:nth-child(2) span[class="sc-d79c9c3f-0 nlmfp sc-cdc9b13f-16 eHyEuD nameCard"]',
         productCountSelector : "#listingCount",
-        productSelector : ".productCard",
+        productCardSelector : ".productCard",
     },
 ]
 
@@ -30,39 +30,40 @@ async function start() {
 
     //TO-DO: limit number of pages concurrently (batches)
     //TO-DO: add novos sites
-    
-    const urlArr = [];
-    for (let { numProductPerPage, mainUrl, productCountSelector, ...productSelectors } of siteArr) {
-        const page = await browser.newPage();
-        await page.setViewport({
-            width: 1600,
-            height: 1200,
-        });
-        const numPages = await getNumPages(page, productCountSelector, numProductPerPage, mainUrl);
-        page.close();
-        console.log("Number of pages: ", numPages);
 
-        urlArr.push(...getUrlArr(numPages, mainUrl, productSelectors));
-    }
+    const urlsToExtractData = await getAllSitesUrls(browser, siteArr);
 
-    const result = await Promise.all(urlArr.map(elem => extracPageData(browser, elem.url, elem.productTextSelector, elem.productSelector)));
+    const result = await Promise.all(urlsToExtractData.map(elem => extracPageData(browser, elem.url, elem.productTextSelector, elem.productCardSelector)));
 
     console.log("Final Result: ", result);
 
     await browser.close();
 }
 
-function getUrlArr(numPages: number, mainUrl: string, productSelectors: any): any[]{
-    const urlArr = [];
-    for (let pageNum = 1; pageNum <= numPages; pageNum++) {
-        const url = mainUrl.replace("PAGE_NUM", `${pageNum}`);
-        urlArr.push({url, ...productSelectors});
+async function getAllSitesUrls(browser: Browser, siteArr: any[]): Promise<any[]>{
+    const sitesUrls = [];
+
+    for (let { numProductPerPage, mainUrl, productCountSelector, ...productSelectors } of siteArr) {
+        const numPages = await getNumPages(browser, productCountSelector, numProductPerPage, mainUrl);
+        console.log("Number of pages: ", numPages);
+
+        sitesUrls.push(...getSiteUrls(numPages, mainUrl, productSelectors));
     }
 
-    return urlArr;
+    return sitesUrls;
 }
 
-async function extracPageData(browser: Browser ,url: string, productTextSelector: string, productSelector: string){
+function getSiteUrls(numPages: number, mainUrl: string, productSelectors: any): any[]{
+    const siteUrls = [];
+    for (let pageNum = 1; pageNum <= numPages; pageNum++) {
+        const url = mainUrl.replace("PAGE_NUM", `${pageNum}`);
+        siteUrls.push({url, ...productSelectors});
+    }
+
+    return siteUrls;
+}
+
+async function extracPageData(browser: Browser ,url: string, productTextSelector: string, productCardSelector: string){
     const page = await browser.newPage();
     await page.setViewport({
         width: 1600,
@@ -71,26 +72,25 @@ async function extracPageData(browser: Browser ,url: string, productTextSelector
 
     await page.goto(url);
 
-
-    const listSize = await listLength(page, productSelector);
+    const listSize = await listLength(page, productCardSelector);
     console.log("List size: ", listSize, "- URL: ", url);
 
-    const productInfoSelArr = getProductInfoSelArr(productTextSelector, listSize);
-    const res = await Promise.all(productInfoSelArr.map(sel => extractProductsData(page, sel)));
+    const productInfoSelectors = getProductInfoSelelectors(productTextSelector, listSize);
+    const res = await Promise.all(productInfoSelectors.map(sel => extractProductData(page, sel)));
 
     await page.close();
 
     return res;
 }
 
-function getProductInfoSelArr(mainProductInfoSel: string, listSize: number): string[]{
-    const selArr = [];
+function getProductInfoSelelectors(mainProductInfoSel: string, listSize: number): string[]{
+    const productInfoSelectors = [];
     for (let i = 1; i <= listSize; i++) {
         const productInfoSel = mainProductInfoSel.replace("INDEX", `${i}`);
-        selArr.push(productInfoSel);
+        productInfoSelectors.push(productInfoSel);
     }
 
-    return selArr;
+    return productInfoSelectors;
 }
 
 async function listLength(page: Page, pageSelector: string): Promise<number>{
@@ -99,7 +99,7 @@ async function listLength(page: Page, pageSelector: string): Promise<number>{
     }, pageSelector);
 }
 
-async function extractProductsData(page: Page, pageSelector: string): Promise<string>{
+async function extractProductData(page: Page, pageSelector: string): Promise<string>{
     const value =  await page.evaluate((selector) => {
         return document.querySelector(selector)?.textContent;
     }, pageSelector);
@@ -110,7 +110,13 @@ async function extractProductsData(page: Page, pageSelector: string): Promise<st
 }
 
 
-async function getNumPages(page: Page, pageSelector: string, numProductPerPage: number, mainUrl: string): Promise<number>{
+async function getNumPages(browser: Browser, pageSelector: string, numProductPerPage: number, mainUrl: string): Promise<number>{
+    const page = await browser.newPage();
+    await page.setViewport({
+        width: 1600,
+        height: 1200,
+    });
+
     const initialUrl = mainUrl.replace("PAGE_NUM", "1");
     await page.goto(initialUrl);
 
@@ -120,86 +126,15 @@ async function getNumPages(page: Page, pageSelector: string, numProductPerPage: 
 
     console.log("Num Pages TEXT: ", text);
 
+    await page.close();
+
     if(!text) throw new Error("Number of pages not found");
 
     const num = Number(text.split(" ")[0]);
 
+
     return Math.ceil(num/numProductPerPage);
 }
-
-//Start 2 - Paginas e browsers em paralelo
-/*
-const PVUrl = "https://www.kabum.com.br/hardware/placa-de-video-vga?page_number=PAGE_NUM&page_size=100&facet_filters=&sort=most_searched"
-async function start(){
-    const browser = await puppeteer.launch({headless: false});
-    const page = await browser.newPage();
-    await page.setViewport({
-        width: 1600,
-        height: 1200,
-    });
-
-    const numPages = await getNumPages(page, productCountSelector);
-    console.log("Products Count: ", numPages);
-
-    const ctxArr = [];
-    // for (let pageNum = 1; pageNum <= numPages; pageNum++) {
-    for (let pageNum = 1; pageNum <= 5; pageNum++) {
-        ctxArr.push(createContext(browser, pageNum));
-    }
-
-    const res = await Promise.all(ctxArr);
-
-    await browser.close();
-
-    console.log("===> Result:  ", res);
-}
-
-async function createContext(browser: Browser, pageNum: number) {
-    const context = await browser.createIncognitoBrowserContext();
-    const page = await context.newPage();
-    await page.setViewport({
-        width: 1600,
-        height: 1200,
-    });
-
-    const url = PVUrl.replace("PAGE_NUM", `${pageNum}`);
-
-    await page.goto(url);
-    const listSize = await listLength(page, productSelector);
-
-    const resArr = []
-    // for (let i = 1; i <= listSize; i++) {
-    for (let i = 1; i <= 5; i++) {
-        const productTextSel = productTextSelector.replace("INDEX", `${i}`);
-        resArr.push(getInfo(productTextSel, context, url));
-    }
-
-    const res = await Promise.all(resArr);
-
-    await context.close();
-
-    console.log("Result Context: ", res);
-
-    return res;
-}
-
-async function getInfo(productTextSel: string, context: BrowserContext, url: any) {
-    const page = await context.newPage();
-    await page.setViewport({
-        width: 1600,
-        height: 1200,
-    });
-    await page.goto(url);
-
-    await page.click(productTextSel);
-
-    const productUrl = page.url();
-
-    await page.close();
-
-    return productUrl;
-}
-*/
 
 start();
 
