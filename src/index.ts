7b9ead027e6531fdc59a23d39b5988cdd7df9f78
gustavovenerando <gvhenrique@hotmail.com>
2023-12-08 00:17:26 -0300

    v0.2.5: added new site gkinfostore, new pagination handling to know number of pages

--- src/index.ts
+++ src/index.ts
@@ -11,26 +11,40 @@ const siteArr = [
         site: "pichau",
         numProductPerPage: 36,
         mainUrl: "https://www.pichau.com.br/hardware/placa-de-video?page=PAGE_NUM",
-        productTextSelector: 'div.MuiGrid-grid-xs-6:nth-child(INDEX) > a:nth-child(1) > div:nth-child(1) > div:nth-child(3) > h2:nth-child(1)',
         productCountSelector : "div.MuiGrid-grid-lg-10 > div:nth-child(1) > div > div:nth-child(1) > div:nth-child(1) > div",
         productCardSelector : 'a[data-cy="list-product"]',
+        productTextSelector: 'div.MuiGrid-grid-xs-6:nth-child(INDEX) > a:nth-child(1) > div:nth-child(1) > div:nth-child(3) > h2:nth-child(1)',
     },
     {
         site: "kabum",
         numProductPerPage: 100,
         mainUrl: "https://www.kabum.com.br/hardware/placa-de-video-vga?page_number=PAGE_NUM&page_size=100&facet_filters=&sort=most_searched",
-        productTextSelector: 'div.sc-cdc9b13f-7:nth-child(INDEX) > a:nth-child(2) > div:nth-child(2) span[class="sc-d79c9c3f-0 nlmfp sc-cdc9b13f-16 eHyEuD nameCard"]',
         productCountSelector : "#listingCount",
         productCardSelector : ".productCard",
+        productTextSelector: 'div.sc-cdc9b13f-7:nth-child(INDEX) > a:nth-child(2) > div:nth-child(2) span[class="sc-d79c9c3f-0 nlmfp sc-cdc9b13f-16 eHyEuD nameCard"]',
+    },
+    {
+        site: "gkinfostore",
+        numProductPerPage: 40,
+        mainUrl: "https://www.gkinfostore.com.br/placa-de-video?pagina=PAGE_NUM",
+        productCountSelector : null,
+        paginationSelector : ".ordenar-listagem.rodape.borda-alpha .pagination > ul",
+        productCardSelector : "#corpo #listagemProdutos .listagem-item",
+        productTextSelector: '#corpo #listagemProdutos > ul > li:nth-child(INDEX) .info-produto > a',
     },
 ]
 
 async function start() {
     const browser = await puppeteer.launch({ headless: false });
 
-    //TO-DO: limit number of pages concurrently (batches)
     //TO-DO: add tratativa de erro (try catch)
     //TO-DO: add novos sites
+    //TO-DO: add novos selectors para preco parcelado, preco a vista e link do produto
+    //TO-DO: add tratativa regex para conseguir o modelo do produto do titulo
+    //TO-DO: refatorar funcao numPages
+    //TO-DO: refatorar selectors da kabum e da pichau
+    //TO-DO: refatorar numProductPerPage para dentro da funcao numPages, não precisa ser chumbado no objeto
+    //       (soh é utilizado quando há productCountSelector) 
 
     const allSitePagesInfoToExtractData = await getAllSitesPagesInfo(browser, siteArr);
 
@@ -62,8 +76,8 @@ function sliceArrayIntoChunks(arr:any[], chunkSize:number){
 async function getAllSitesPagesInfo(browser: Browser, siteArr: any[]): Promise<any[]>{
     const sitesPagesInfo = [];
 
-    for (let { numProductPerPage, mainUrl, productCountSelector, ...productSelectors } of siteArr) {
-        const numPages = await getNumPages(browser, productCountSelector, numProductPerPage, mainUrl);
+    for (let { numProductPerPage, mainUrl, productCountSelector, paginationSelector, ...productSelectors } of siteArr) {
+        const numPages = await getNumPages(browser, productCountSelector,paginationSelector, numProductPerPage, mainUrl);
         console.log("Number of pages: ", numPages);
 
         sitesPagesInfo.push(...getSitePagesInfo(numPages, mainUrl, productSelectors));
@@ -130,7 +144,7 @@ async function extractProductData(page: Page, pageSelector: string): Promise<str
 }
 
 
-async function getNumPages(browser: Browser, pageSelector: string, numProductPerPage: number, mainUrl: string): Promise<number>{
+async function getNumPages(browser: Browser, productCountSelector: string | null, paginationSelector: string,numProductPerPage: number, mainUrl: string): Promise<number>{
     const page = await browser.newPage();
     await page.setViewport({
         width: 1600,
@@ -140,11 +154,26 @@ async function getNumPages(browser: Browser, pageSelector: string, numProductPer
     const initialUrl = mainUrl.replace("PAGE_NUM", "1");
     await page.goto(initialUrl);
 
-    const text = await page.evaluate((selector) => {
-        return document.querySelector(selector)?.textContent;
-    }, pageSelector);
+    let text;
 
-    console.log("Num Pages TEXT: ", text);
+    if (!productCountSelector) {
+        text = await page.evaluate((selector) => {
+            const paginationElem = document.querySelector(selector);
+            const paginagionChildren = paginationElem?.children;
+
+            if(!paginagionChildren) throw new Error("Pagination children not found");
+
+            const lastPageElem = paginagionChildren[paginagionChildren.length - 2];
+
+            return lastPageElem.textContent;
+        }, paginationSelector);
+
+        return Number(text);
+    } 
+
+    text = await page.evaluate((selector) => {
+        return document.querySelector(selector)?.textContent;
+    }, productCountSelector);
 
     await page.close();
 
